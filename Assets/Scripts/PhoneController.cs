13eca3368da127a971617bb0f1deaa1aba6f6e8e
Azin A <aalahkhani@gmail.com>
2023-12-14 23:18:19 +0000

    connect start mobile phone screen to options

--- Assets/Scripts/PhoneController.cs
+++ Assets/Scripts/PhoneController.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhoneController : MonoBehaviour
 {
     public GameObject phoneGameObject;
 
-    
-   public void OnTriggerEnter(Collider other)
+    public Button shopButton;
+    public Button moneyButton;
+    public Button temperatureButton;
+
+    public GameObject shopListObj;
+    public GameObject moneyListObj;
+    public GameObject temperatureListObj;
+
+    private void OnEnable()
+    {
+        shopButton.onClick.AddListener(ActivateShopList);
+        moneyButton.onClick.AddListener(ActivateMoneyList);
+        temperatureButton.onClick.AddListener(ActivateTemperatureList);
+    }
+    private void OnDisable()
+    {
+        shopButton.onClick.RemoveAllListeners();
+        moneyButton.onClick.RemoveAllListeners();
+        temperatureButton.onClick.RemoveAllListeners();
+    }
+    public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -22,5 +42,25 @@ public class PhoneController : MonoBehaviour
             phoneGameObject.SetActive(false);
 
         }
+    }
+    public void ActivateShopList()
+    {   
+        shopListObj.SetActive(true);    
+        moneyListObj.SetActive(false);
+        temperatureListObj.SetActive(false);    
+
+    }
+    public void ActivateMoneyList()
+    {
+        moneyListObj.SetActive(true) ;  
+        temperatureListObj.SetActive(false) ;
+        shopListObj.SetActive(false) ;
+
+    }
+    public void ActivateTemperatureList()
+    {
+        moneyListObj.SetActive(false);
+        temperatureListObj.SetActive(true);
+        shopListObj.SetActive(false);
     }
 }
