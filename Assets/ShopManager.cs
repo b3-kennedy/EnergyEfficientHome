b59ef44bae65a81960fbfad11c5f5b81a85ff30e
Azin A <aalahkhani@gmail.com>
2023-12-14 20:09:01 +0000

    add first item to shop

---
+++ Assets/ShopManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public GameObject[] shopItems;
+    public Texture[] itemImages;
+    public string[] itemInfo;
+    public string[] itemNames;
+    public float[] itemPrices;
+
+    private ShopItem[] Basket;
+
+    private void OnEnable()
+    {
+          for(int i = 0; i< shopItems.Length;i++)
+        {
+
+            shopItems[i].GetComponent<ShopItem>().buyBtn.onClick.AddListener(Add);
+           
+           
+        }
+    }
+    
+    private void Start()
+    {
+        for (int i = 0; i < shopItems.Length; i++)
+        {
+            
+            ShopItem item = shopItems[i].GetComponent<ShopItem>();
+            if (itemImages[i] != null)
+                shopItems[i].GetComponent<ShopItem>().SetItemImage(itemImages[i]);
+            if (itemNames[i] != null && itemInfo[i] != null && itemPrices[i] != 0)
+                shopItems[i].GetComponent<ShopItem>().SetItemInfo(itemPrices[i], itemInfo[i], itemNames[i]);
+            item = shopItems[i].GetComponent<ShopItem>();
+            Debug.Log(shopItems[i].GetComponent<ShopItem>().name);
+
+        }
+    }
+    public void Add() {
+        
+
+    }
+
+}
