5911cfc87c5d286ba8d94b14323ac96760d0841a
Jameuwe <thtimes.s@gmail.com>
2024-03-09 23:50:53 +0000

    finish exercise mini game

---
+++ Assets/MiniGame/Noted.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noted : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed;
+    public bool canPress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveRight();
+        if (Input.GetKeyDown(KeyCode.Space) && canPress)
+        {
+            deactivated();
+            ScoreManager.Instance.addScore();
+            canPress = false;
+        }
+    }
+
+    public void moveRight()
+    {
+        rb.velocity = -transform.right * speed;
+    }
+    public void deactivated()  
+    { 
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Activator"))
+        {
+            canPress = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Activator"))
+        {
+            canPress = false;
+        }
+    }
+}
