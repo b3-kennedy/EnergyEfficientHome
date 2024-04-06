78468fdb5fc3e4455bc5d95bda05134ff2558ac2
b3-kennedy <benjamin3.kennedy@live.uwe.ac.uk>
2024-04-06 19:50:13 +0100

    made it more clear when radiators or windows are broken

--- Assets/Broken.cs
+++ Assets/Broken.cs
@@ -7,17 +7,40 @@ public class Broken : MonoBehaviour
 {
 
 
-    public GameObject brokenIndicator;
     GameObject indicator;
     public float yHeight;
     [HideInInspector] public UnityEvent enable;
     [HideInInspector] public UnityEvent disable;
     public MonoBehaviour scriptToDisable;
+    public Transform indicatorPos;
     public float fixCost;
+    public GameObject windowIndicator;
+    public GameObject radiatorIndicator;
+
+    public float windowScaleX;
+    public float windowScaleY;
+    public float windowScaleZ;
+
+    public float radiatorScaleX = 5;
+    public float radiatorScaleY = 2.5f;
+    public float radiatorScaleZ = 1;
+        
 
     private void OnEnable()
     {
-        indicator = Instantiate(brokenIndicator, new Vector3(transform.position.x, transform.position.y + yHeight, transform.position.z), Quaternion.identity);
+        if (GetComponent<Window>())
+        {
+            indicator = Instantiate(windowIndicator, indicatorPos.position, transform.rotation);
+            indicator.transform.localScale = new Vector3(transform.localScale.x * windowScaleX, transform.localScale.y * windowScaleY, transform.localScale.z * windowScaleZ);
+
+        }
+        else if (GetComponent<Radiator>())
+        {
+            indicator = Instantiate(radiatorIndicator, indicatorPos.position, transform.rotation);
+            indicator.transform.localScale = new Vector3(transform.localScale.x * radiatorScaleX, transform.localScale.y * radiatorScaleY, transform.localScale.z * radiatorScaleZ);
+        }
+        
+        //indicator.transform.SetParent(transform);
         enable.Invoke();
         UIManager.Instance.DisplayNotification(("A " + GetComponent<RoomTempChanger>().objectName + " HAS BROKEN IN THE " + transform.parent.name).ToUpper());
         if (GetComponent<Radiator>())
